59268b05793876e1897a84e380dcba787d01d31e
Mateo Bošnjak <bosnjakmateo1@gmail.com>
2024-08-25 20:06:51 +0200

    Added parser support functions

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 type Node interface {
@@ -210,24 +211,53 @@ type IfExpression struct {
 	Alternative *BlockStatement
 }
 
-func (b *IfExpression) expressionNode() {
+func (i *IfExpression) expressionNode() {
 
 }
-func (b *IfExpression) TokenLiteral() string {
-	return b.Token.Literal
+func (i *IfExpression) TokenLiteral() string {
+	return i.Token.Literal
 }
-func (b *IfExpression) String() string {
+func (i *IfExpression) String() string {
 	var out bytes.Buffer
 
 	out.WriteString("if")
-	out.WriteString(b.Condition.String())
+	out.WriteString(i.Condition.String())
 	out.WriteString(" ")
-	out.WriteString(b.Consequence.String())
+	out.WriteString(i.Consequence.String())
 
-	if b.Alternative != nil {
+	if i.Alternative != nil {
 		out.WriteString("else ")
-		out.WriteString(b.Alternative.String())
+		out.WriteString(i.Alternative.String())
 	}
+
+	return out.String()
+}
+
+type FunctionalLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionalLiteral) expressionNode() {
+
+}
+func (fl *FunctionalLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+func (fl *FunctionalLiteral) String() string {
+	var out bytes.Buffer
+
+	var params []string
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
